d20f225889dacd5553d9b71e00eaad7bd7e05d33
kk <1694681761@qq.com>
2022-08-15 17:22:16 +0800

    1,28,29

---
+++ default_29pro(14)/static/js/thea2.js
@@ -0,0 +1,1 @@
+document.write("	<div class=\"left-ad\">            <div style=\"display: block;background-color: #fff;border: 2px dashed #bbb;text-align: center;color: #aaa;line-height:60px;font-size: 15px;/*width:730px;*//*margin-left:15px;*/\">              <div style=\"text-align: center;line-height:60px;\">首页广告位二-580PX*60PX</div>            </div>          </div>")
