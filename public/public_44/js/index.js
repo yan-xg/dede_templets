1f48edf2f9fb7d728fd20f2ff6f60691be9fcefe
kk <1694681761@qq.com>
2022-09-27 16:41:19 +0800

    default_44

---
+++ public/public_44/js/index.js
@@ -0,0 +1,73 @@
+
+$(document).ready(function () {
+ var mySwiper = new Swiper('#swiper-container1',{
+	slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	})
+	var mySwiper2 = new Swiper('#swiper-container2',{
+	slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	})
+	var mySwiper3 = new Swiper('#swiper-container3',{
+	slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	})
+	var mySwiper4 = new Swiper('#swiper-container4',{
+	slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	})
+	var mySwiper5 = new Swiper('#swiper-container5',{
+	slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+	})
+})
+
+
+
+
+	
